e33bb6009bd7906412a48ba14a0e466823b9a222
Fluksy <sellier.thomas@hotmail.fr>
2021-11-01 20:59:51 +0100

    [component] add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,3 @@
+import './Footer.scss';
+
+export const Footer = () => <div className="Footer d-flex justify-content-center align-items-center">Made with ❤️ by Fluxy</div>
---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { Footer } from './Footer';
+
+test('renders footer', () => {
+  render(<Footer />);
+  const madeWithLove = screen.getByText(/Made with ❤️ by Fluxy/i);
+  expect(madeWithLove).toBeInTheDocument();
+});
