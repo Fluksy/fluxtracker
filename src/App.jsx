7b8d7b71ce47629d429e8ec02d061398ee4f14e9
Fluksy <sellier.thomas@hotmail.fr>
2021-11-01 21:00:41 +0100

    homepage using header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Home } from './components/Home/Home';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='d-flex flex-column min-vh-100'>
+		<Header />
+    <Home></Home>
+    <Footer />
+  </div>
 }
 
 export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+
+    return <div className="container ml-5 mr-5 flex-grow-1">
+		content
+	</div>
+
+}
