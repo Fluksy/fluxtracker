bfe58b194175c7283897690da5a89c9b10850e36
Fluksy <sellier.thomas@hotmail.fr>
2022-01-28 01:01:44 +0100

    [vercel] configuration des serverless functions

--- src/config/api.fluxtracker.js
+++ src/config/api.fluxtracker.js
@@ -1,17 +1,13 @@
 import * as axios from 'axios';
 
 const fluxtrackerApi = axios.create({
-	baseURL: 'https://api.fluxtracker.fr'
+	baseURL: '/api'
 })
 
-// Fetch delegation data from Elrond API.
-// Parameter can be an erd1 address
 export const getProviders = async(erd1Address) => {
-	return fluxtrackerApi.get(`/accounts/${erd1Address}/delegations`);
+	return fluxtrackerApi.get(`/provider/${erd1Address}`);
 }
 
-// Fetch Service Provider data from Elrond API.
-// Parameter is a smart contract address
 export const getProviderInfos = async(serviceProviderSmartContractAddress) => {
-	return fluxtrackerApi.get(`/providers/${serviceProviderSmartContractAddress}`);
+	return fluxtrackerApi.get(`/providersInfos/${serviceProviderSmartContractAddress}`);
 }
