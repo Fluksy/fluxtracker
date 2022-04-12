ac980ed2c0e6dc1cceea05eb74e3a437c3391092
Fluksy <sellier.thomas@hotmail.fr>
2022-04-12 22:04:36 +0200

    Add some tooltips in card

--- src/components/ServiceProviderCard/ServiceProviderCard.jsx
+++ src/components/ServiceProviderCard/ServiceProviderCard.jsx
@@ -3,6 +3,7 @@ import { FaTwitter, FaGithub, FaLink, FaServer, FaPercent } from "react-icons/fa
 import { GoVerified } from "react-icons/go";
 import { Card, CardBody, CardFooter, CardHeader, CardImg, CardText, CardTitle } from "reactstrap";
 import { getProviderInfos } from "../../config/api.fluxtracker";
+import ReactTooltip from 'react-tooltip';
 import './ServiceProviderCard.scss';
 
 const LOGO_FONT_SIZE = 22;
@@ -23,39 +24,40 @@ const ServiceProviderCard = (props) => {
 	}, [props.contract])
 
 	return (
-		<Card className="custom-card card border-card">
+		<Card className="custom-card card border-card place-self-center">
 			{loading && !provider ?
 				<div>Loading...</div> :
 				<>
+					<ReactTooltip clickable={true} />	
 					<CardHeader className="d-flex align-items-center">
 						<CardImg src={provider?.identity?.avatar}></CardImg>
 						<CardTitle className="flex-grow-1 text-center">
 							<span>{provider?.identity?.name}</span>
 						</CardTitle>
-						{!!provider?.featured && <GoVerified style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE} />}
+						{!!provider?.featured && <div data-tip={`Verified`} className="p-2 d-flex"><GoVerified size={LOGO_FONT_SIZE} /></div>}
 					</CardHeader>
 					<CardBody>
 						<CardText>{provider?.identity.description}</CardText>
 					</CardBody>
-					<CardFooter className="d-flex align-items-center" style={{gap: '.3em'}}>
-						<div className="d-flex align-items-center flex-grow-1" style={{gap: '.3em'}}>
+					<CardFooter className="d-flex align-items-center justify-content-between flex-wrap" style={{gap: '.5em'}}>
+						<div className="d-flex align-items-center" style={{gap: '.5em'}}>
 							{!!provider?.numNodes && 
-								<>
+								<div data-tip={`Node count`} className="d-flex align-items-center" style={{gap: '.3em'}}>
 									<span>{`${provider?.numNodes}`}</span>
-									<FaServer style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE}/>
-								</>
+									<FaServer size={LOGO_FONT_SIZE}/>
+								</div>
 							}
 							{!!provider?.apr && 
-								<>
+								<div data-tip={`APR: ${provider?.apr} %`} className="d-flex align-items-center" style={{gap: '.3em'}}>
 									<span>{`${provider?.apr}`}</span>
-									<FaPercent style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE}/>
-								</>
+									<FaPercent size={LOGO_FONT_SIZE}/>
+								</div>
 							}
 						</div>
 						<div className="d-flex">
-							{!!provider?.identity?.url && <a className="p-2 d-flex" href={provider?.identity?.url} target='_blank' rel="noopener noreferrer"><FaLink style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE} /></a>}
-							{!!provider?.identity.github && <a className="p-2 d-flex" href={provider?.identity.github} target='_blank' rel="noopener noreferrer"><FaGithub style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE} /></a>}
-							{!!provider?.identity.twitter && <a className="p-2 d-flex" href={provider?.identity.twitter} target='_blank' rel="noopener noreferrer"><FaTwitter style={{color: 'var(--primary-color)'}} size={LOGO_FONT_SIZE} /></a>}
+							{!!provider?.identity?.url && <a data-tip={`Website`} className="p-2 d-flex" href={provider?.identity?.url} target='_blank' rel="noopener noreferrer"><FaLink size={LOGO_FONT_SIZE} /></a>}
+							{!!provider?.identity.github && <a data-tip={`Github`} className="p-2 d-flex" href={provider?.identity.github} target='_blank' rel="noopener noreferrer"><FaGithub size={LOGO_FONT_SIZE} /></a>}
+							{!!provider?.identity.twitter && <a data-tip={`Twitter`} className="p-2 d-flex" href={provider?.identity.twitter} target='_blank' rel="noopener noreferrer"><FaTwitter size={LOGO_FONT_SIZE} /></a>}
 						</div>
 					</CardFooter>
 				</>
