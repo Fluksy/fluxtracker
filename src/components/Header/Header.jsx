7491aaea18bef42dacf69ea5cfb9b7bdb90b534c
Fluksy <sellier.thomas@hotmail.fr>
2022-04-15 00:03:40 +0200

    New navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,29 +7,53 @@ import { connect } from 'react-redux';
 import { setLocale } from 'react-redux-i18n';
 import { KEY_LANGUAGE_LOCAL_STORAGE } from '../../locales'
 
+import { Button, Container, Form, FormControl, Navbar, NavDropdown, Offcanvas, Nav} from 'react-bootstrap';
+
+
 const Header = ({ setLocale, locale }) => {
-	return <div className="Header sticky-top">
-		<nav className="navbar align-items-stretch flex-nowrap px-0">
-			<div className="d-flex align-items-center ms-3 ms-sm-5">
-				<Link to='/search' className="d-flex rounded p-2 align-items-center navbar-brand maiar-shadow">
-					<ElrondLogo />
-					<span className="p-0 ps-sm-3 text-secondary text-truncate">Fluxtracker</span>
-				</Link>
+	return <Navbar className='bg-navbar' expand={false}>
+		<Container fluid>
+			<Link to='/search' className="d-flex rounded p-2 align-items-center navbar-brand maiar-shadow">
+				<ElrondLogo />
+				<span className="p-0 ps-sm-3 text-secondary text-truncate">Fluxtracker</span>
+			</Link>
+			<div className='d-flex'>
+				<ReactFlagsSelect
+					className='pb-0'
+					countries={Object.keys(locales)}
+					showOptionLabel={false}
+					showSelectedLabel={false}
+					selected={locale}
+					style={{ backgroundColor: '#18191a' }}
+					onSelect={(code) => setLocale(code)}
+				/>
+				<Navbar.Toggle aria-controls="offcanvasNavbar" className='ms-2' />
 			</div>
-			<div className="d-flex align-items-center pe-4">
-				<ul className="flex-row navbar-nav">
-					<ReactFlagsSelect
-						countries={Object.keys(locales)}
-						showOptionLabel={false}
-						showSelectedLabel={false}
-						selected={locale}
-						style={{ backgroundColor: '#18191a' }}
-						onSelect={(code) => setLocale(code)}
-					/>
-				</ul>
-			</div>
-		</nav>
-	</div>
+			<Navbar.Offcanvas
+				id="offcanvasNavbar"
+				aria-labelledby="offcanvasNavbarLabel"
+				placement="end"
+			>
+				<Offcanvas.Header closeButton>
+					<Offcanvas.Title id="offcanvasNavbarLabel">Fluxtracker</Offcanvas.Title>
+				</Offcanvas.Header>
+				<Offcanvas.Body>
+					{/* <Nav className="justify-content-end flex-grow-1 pe-3">
+						<Nav.Link href="#action1">Home</Nav.Link>
+						<Nav.Link href="#action2">Link</Nav.Link>
+						<NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+							<NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action5">
+								Something else here
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav> */}
+				</Offcanvas.Body>
+			</Navbar.Offcanvas>
+		</Container>
+	</Navbar>
 }
 
 const mapStateToProps = ({i18n}) => ({locale : i18n.locale})
