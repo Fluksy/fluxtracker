021175da8df09dae44cf4a73b37e63f7e436a026
Fluksy <sellier.thomas@hotmail.fr>
2022-04-25 22:54:50 +0200

    [locales] add Romanian and Spanish

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,11 +1,15 @@
 import en from './en.json';
 import fr from './fr.json';
+import ro from './ro.json';
+import es from './es.json';
 
 export const KEY_LANGUAGE_LOCAL_STORAGE = 'user-language';
 
 export const locales = {
 	GB : en,
-	FR : fr
+	FR : fr,
+	RO : ro,
+	ES : es
 };
 
 export const DEFAULT_LANGUAGE = !!localStorage.getItem(KEY_LANGUAGE_LOCAL_STORAGE) ? localStorage.getItem(KEY_LANGUAGE_LOCAL_STORAGE) :  "GB";
